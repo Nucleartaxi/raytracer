57ca02f170d4077fcbfce2dd6db4785bb9bf1061
Nucleartaxi <alexander@truewheels.net>
2021-06-30 19:28:56 -0700

    wip

--- src/image.rs
+++ src/image.rs
@@ -5,7 +5,7 @@ pub fn create_test_image(v: &mut Vec<u8>, width: u32, height: u32) {
             let r = i as f32 / (width - 1) as f32;
             let g = j as f32 / (height - 1) as f32;
             let b = 0.25;
-            println!("{} {} {}", r, g, b);
+            // println!("{} {} {}", r, g, b);
             
             v[index] = (r * 255.999) as u8;
             index = index + 1;
