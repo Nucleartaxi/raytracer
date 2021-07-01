ef6da2f28a67a087bbd0f1e97c742e2f9da9912e
Nucleartaxi <alexander@truewheels.net>
2021-06-30 19:32:28 -0700

    image output works!

--- src/image.rs
+++ src/image.rs
@@ -1,7 +1,7 @@
 pub fn create_test_image(v: &mut Vec<u8>, width: u32, height: u32) {
     let mut index = 0;
-    for i in (0..(height - 1)).rev() {
-        for j in (0..(width - 1)).rev() {
+    for i in (0..(height)).rev() {
+        for j in 0..width {
             let r = i as f32 / (width - 1) as f32;
             let g = j as f32 / (height - 1) as f32;
             let b = 0.25;
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ mod image;
 
 fn main() {
     println!("Hello, world!");
-    const WIDTH: u32 = 3;
-    const HEIGHT: u32 = 3;
+    const WIDTH: u32 = 256;
+    const HEIGHT: u32 = 256;
     let size = (WIDTH * HEIGHT * 3) as usize;
     let mut v: Vec<u8> = Vec::with_capacity(size);
     v.resize(size, 0);
 
     image::create_test_image(&mut v, WIDTH, HEIGHT);
-    for i in &v {
-        println!("{}", i);
-    }
+    // for i in &v {
+    //     println!("{}", i);
+    // }
     image::output_image(&v, WIDTH, HEIGHT)
 }
