b9ee3bc52e2e38116b36b581f2d614d1bdf85888
Nucleartaxi <alexander@truewheels.net>
2021-06-30 19:26:01 -0700

    creates and writes image

--- src/image.rs
+++ src/image.rs
@@ -1,28 +1,29 @@
 pub fn create_test_image(v: &mut Vec<u8>, width: u32, height: u32) {
-    let mut i = 0;
-    while i < height {
-        let mut j = 0;
-        while j < width {
+    let mut index = 0;
+    for i in (0..(height - 1)).rev() {
+        for j in (0..(width - 1)).rev() {
             let r = i as f32 / (width - 1) as f32;
             let g = j as f32 / (height - 1) as f32;
             let b = 0.25;
+            println!("{} {} {}", r, g, b);
             
-            v.push((r * 255.999) as u8);
-            v.push((g * 255.999) as u8);
-            v.push((b * 255.999) as u8);
-            j = j + 1;
+            v[index] = (r * 255.999) as u8;
+            index = index + 1;
+            v[index] = (g * 255.999) as u8;
+            index = index + 1;
+            v[index] = (b * 255.999) as u8;
+            index = index + 1;
         }
-        i = i + 1;
     }
 }
 
-pub fn output_image(v: &mut Vec<u8>, width: u32, height: u32) {
+pub fn output_image(image: &[u8], width: u32, height: u32) {
     //for writing and opening files
     use std::path::Path;
     use std::fs::File;
     use std::io::BufWriter;
 
-	let path = Path::new("target/image/image.png");
+	let path = Path::new("image.png");
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
 
@@ -31,5 +32,5 @@ pub fn output_image(v: &mut Vec<u8>, width: u32, height: u32) {
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
 
-    // writer.write_image_data(v).unwrap();
+    writer.write_image_data(image).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ mod image;
 
 fn main() {
     println!("Hello, world!");
-    const WIDTH: u32 = 2;
-    const HEIGHT: u32 = 1;
-    // let size = WIDTH * HEIGHT * 3;
-    // let a: [u8; size];
-    let mut v: Vec<u8> = Vec::new();
+    const WIDTH: u32 = 3;
+    const HEIGHT: u32 = 3;
+    let size = (WIDTH * HEIGHT * 3) as usize;
+    let mut v: Vec<u8> = Vec::with_capacity(size);
+    v.resize(size, 0);
+
     image::create_test_image(&mut v, WIDTH, HEIGHT);
-    for i in v {
+    for i in &v {
         println!("{}", i);
     }
+    image::output_image(&v, WIDTH, HEIGHT)
 }
