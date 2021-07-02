8d1d359ff7d913887a6a8b5f016c81e0e5d25c65
Nucleartaxi <alexander@truewheels.net>
2021-07-02 13:24:37 -0700

    removed pre-sized vector, vector is now default size of 0 at start

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ fn main() {
     println!("Hello, world!");
     const WIDTH: u32 = 256;
     const HEIGHT: u32 = 256;
-    let size = (WIDTH * HEIGHT * 3) as usize;
-    let mut v: Vec<u8> = Vec::with_capacity(size);
-    v.resize(size, 0);
+    let mut v: Vec<u8> = Vec::new();
 
     image::create_test_image(&mut v, WIDTH, HEIGHT);
     // for i in &v {
