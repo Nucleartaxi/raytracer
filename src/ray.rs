ad37f207cd1f749b205a1bd3843038ce4f5bc0de
Nucleartaxi <alexander@truewheels.net>
2021-07-06 15:50:16 -0700

    clarified code in ray_color method

--- src/ray.rs
+++ src/ray.rs
@@ -27,7 +27,10 @@ impl Ray {
     pub fn ray_color(&self) -> color::Color { //does math with vectors, then returns a color
         let unit_direction = self.direction.unit_vector();
         let t = 0.5 * (unit_direction.y() + 1.0);
-        let temp = vec3::Vec3::new(1.0, 1.0, 1.0).multiply_by(1.0 - t).add(&vec3::Vec3::new(0.5, 0.7, 1.0).multiply_by(t));
+        let start_value = vec3::Vec3::new(1.0, 1.0, 1.0);
+        let end_value = vec3::Vec3::new(0.5, 0.7, 1.0);
+        let temp = start_value.multiply_by(1.0 - t).add(&end_value.multiply_by(t));
+        // let temp = vec3::Vec3::new(1.0, 1.0, 1.0).multiply_by(1.0 - t).add(&vec3::Vec3::new(0.5, 0.7, 1.0).multiply_by(t));
         color::Color::new(temp.x(), temp.y(), temp.z())
     }
 }
